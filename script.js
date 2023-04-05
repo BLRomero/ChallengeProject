970c0cdaf493743ab4e88c5e0a7f4b7487dbff37
Bonnette Romero <82954485+BLRomero@users.noreply.github.com>
2023-04-05 17:05:22 -0500

    fixed all glitches and game works well.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-var count ;
 class MixOrMatch {
   constructor(totalTime, cards) {
     this.cardsArray = cards;
@@ -15,50 +14,42 @@ class MixOrMatch {
     this.timeRemaining = this.totalTime;
     this.matchedCards = [];
     this.busy = true;
-    var count = 0;
 
     setTimeout(() => {
       this.shuffleCards();
-      this.countDown = this.startCountdown();
+      this.countDown = this.startCountdown()
       this.busy = false;
-    }, 500);
+    }, 500 , this);
 
     this.hideCards();
     this.timer.innerText = this.timeRemaining;
     this.ticker.innerText = this.totalClicks;
   }
+
   hideCards() {
     this.cardsArray.forEach((card) => {
       card.classList.remove("visible");
-      card.classList.remove("matched");     
+      card.classList.remove("matched");
     });
   }
 
   flipCard(card) {
     let canFlip = this.canFlipCard(card);
-    if (canFlip){
+    if (canFlip) {
       this.totalClicks++;
       this.ticker.innerText = this.totalClicks;
-      this.flip();
       card.classList.add("visible");
+      card.classList.add("animals");
 
-      if (this.cardToCheck) this.checkForCardMatch(card);
-      else this.cardToCheck = card;
+
+      if (this.cardToCheck) {this.checkForCardMatch(card);}
+      else {this.cardToCheck = card;}
     }
   }
-  
-
-flip() {
-  // document.getElementById("img1").src="./images/chickensbarn2.jpg";
-}
 
   checkForCardMatch(card) {
-    let card1 = this.getCardType(card);
-    let card2 = this.getCardType(this.cardToCheck);
-    console.log(card1, card2)
-    if (card1 === card2){
+    if (this.getCardType(card) === this.getCardType(this.cardToCheck))
       this.cardMatch(card, this.cardToCheck);
-    }
     else this.cardMismatch(card, this.cardToCheck);
 
     this.cardToCheck = null;
@@ -73,23 +64,16 @@ flip() {
   }
 
   cardMismatch(card1, card2) {
-    console.log("card mismatch")
     this.busy = true;
-    resetCard();
     setTimeout(() => {
       card1.classList.remove("visible");
       card2.classList.remove("visible");
       this.busy = false;
-      
     }, 1000);
   }
 
   getCardType(card) {
-    // return card.getElementsByClassName("card-front")[0].src;
-    let cardName = "frontimg_" + count;
-    console.log(cardName);
-    return document.getElementById(cardName).src;
-
+    return card.getElementsByClassName("card-value")[0].src;
   }
 
   startCountdown() {
@@ -122,52 +106,32 @@ flip() {
 
   canFlipCard(card) {
     return (
-      !this.busy && 
-      !this.matchedCards.includes(this.cardsArray) && 
+      !this.busy &&
+      !this.matchedCards.includes(this.cardsArray) &&
       card !== this.cardToCheck);
   }
 }
 
-// function ready() {
+function ready() {
   let overlays = Array.from(document.getElementsByClassName("overlay-text"));
   let cards = Array.from(document.getElementsByClassName("card"));
   let game = new MixOrMatch(100, cards);
-   game.startGame();
+  game.startGame();
   overlays.forEach((overlay) => {
     overlay.addEventListener("click", () => {
       overlay.classList.remove("visible");
-     
     });
   });
-  
+
   cards.forEach((card) => {
     card.addEventListener("click", () => {
-      console.log("Card Click")
       game.flipCard(card);
     });
   });
-// }
+}
 
-// if (document.readyState == "loading") {
-//   document.addEventListener("DOMContentLoaded", ready);
-// } else {
-//   ready();
-// }
-
-function onclickimg(numCard){
-    count = numCard;
-    console.log("click", numCard);
-    let imageClass = "backimg_" + numCard;
-    document.getElementById(imageClass).className="clickCard";
-    let frontimg ="frontimg_" + numCard;
-    document.getElementById(frontimg).className="card-front";
-    
-  }
-
-  function resetCard(){
-    console.log('reset');
-    for(let i = 0; i < 16; i++) {
-      let imgClass = "backimg_" + i;
-      document.getElementById(imgClass).className="cardReset";
-    }
-  }
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
